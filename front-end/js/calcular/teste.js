ccfb1e42012d6e4289767c012f54985f7a012670
Kevin Menezes Herrera <kevinmenezesherrera@gmail.com>
2023-06-24 10:48:41 -0300

    teste e refatoracao concluidos, falta implementar

--- front-end/js/calcular/teste.js
+++ front-end/js/calcular/teste.js
@@ -1,6 +1,74 @@
 const comprimentoBarra = 3;
 
+function trataNumeros(numero) {
+	const string = numero + "" //converte para String
+	const strNumber = Number(string) //guarda o valor de string convertido para number
+
+	if (Number.isInteger(strNumber) === false) {
+		if (string.indexOf('.') !== -1 || string.indexOf(',') !== -1) {
+			//quando tem inteiros e frações . ou ,
+			//substitui , por .
+			let stringTemp = string.replace(',', '.')
+			//pega o o numero do indice onde está o ponto .
+			const index = stringTemp.indexOf('.')
+			//separa os inteiros
+			const inteiroTemp = stringTemp.slice(0, index);
+			//separa fração
+			const fracaoTemp = stringTemp.slice(index + 1, stringTemp.length);
+
+			const inteiro = Number(inteiroTemp);
+			const fracao = Number(fracaoTemp);
+
+			return { inteiro, fracao }
+		}
+	} else if (Number.isInteger(strNumber) === true) {
+		return strNumber;
+	}
+}
+
+function calculaSobra(tamanho) {
+	const inteiro = trataNumeros(tamanho).inteiro;//pega a parte inteira
+	const tamanhoModBarra = tamanho % comprimentoBarra;//pega o tamanho passado e tira o modulo do comprimento da barra
+	const inteiroModBarra = inteiro % comprimentoBarra;//pega o inteiro passado e tira o modulo do comprimento da barra
+
+	if (Number.isInteger(tamanho) === true) { //se o numero for inteiro
+		if (tamanhoModBarra === 0) {
+			return 0
+		} else {
+			sobra = comprimentoBarra - tamanhoModBarra
+			return sobra
+		}
+	} else if (Number.isInteger(tamanho) === false) {//se o numero nao for inteiro
+		const proxDivisor3 = inteiro + (comprimentoBarra - inteiroModBarra)
+		const metrosUsadosBarra = proxDivisor3 - tamanho;
+		sobra = metrosUsadosBarra
+		sobra = sobra.toFixed(2)
+		return sobra
+	}
+}
+
 function calculaBarra(tamanho) {
+	tamanho = Number(tamanho)
+	let qtdBarra;
+
+	if (tamanho === 0) {
+		return console.error('IMPOSSIVEL FAZER A CONTA COM 0 (ZERO) METROS');
+	} else if (tamanho <= 3 && tamanho > 0 || tamanho === 1) {
+		return 1;
+	} else if (tamanho % comprimentoBarra === 0) {
+		qtdBarra = tamanho / comprimentoBarra;
+		return qtdBarra
+	} else {
+		qtdBarra = Math.ceil(tamanho / comprimentoBarra);
+		return qtdBarra;
+	}
+}
+
+console.log(calculaBarra(15.01));
+
+/**!SECTION
+ * 
+ * function calculaBarra(tamanho) {
 	tamanho = Number(tamanho)
 	let qtdBarra;
 	//let inteiro = Number.isInteger(tamanho)
@@ -26,67 +94,4 @@ function calculaBarra(tamanho) {
 
 }
 
-function trataNumeros(numero) {
-	const string = numero + "" //converte para String
-	const strNumber = Number(string) //guarda o valor de string convertido para number
-
-	if (Number.isInteger(strNumber) === false) {
-		if (string.indexOf('.') !== -1 || string.indexOf(',') !== -1) {
-			//quando tem inteiros e frações . ou
-			//substitui , por .
-			let stringTemp = string.replace(',', '.')
-			//pega o o numero do indice onde está o ponto .
-			const index = stringTemp.indexOf('.')
-			//separa os inteiros
-			const inteiroTemp = stringTemp.slice(0, index);
-			//separa fração
-			const fracaoTemp = stringTemp.slice(index + 1, stringTemp.length);
-
-			const inteiro = Number(inteiroTemp);
-			const fracao = Number(fracaoTemp);
-
-			return { inteiro, fracao }
-		}
-	} else if (Number.isInteger(strNumber) === true) {
-		return strNumber;
-	}
-}
-
-function calculaSobra(tamanho) {
-	const number = trataNumeros(tamanho);
-	let sobra = 0;
-
-	if (Number.isInteger(tamanho) === true) {
-	//colocar a logica para um numero inteiro
-		console.log('é inteiro');
-		if (tamanho % comprimentoBarra === 0) {
-			return 0
-		} else {
-			sobra = comprimentoBarra - (tamanho % comprimentoBarra)
-			return sobra
-		}
-	} else if (Number.isInteger(tamanho) === false) {
-		console.log('não é inteiro');
-		let cont = 0;
-		if (trataNumeros(tamanho).inteiro % comprimentoBarra === 0) {
-			cont = comprimentoBarra
-		} else {
-			for (let x = trataNumeros(tamanho).inteiro; (x % comprimentoBarra !== 0); x++) {
-				cont += 1;
-			}
-		}
-		const proxDivisor3 = trataNumeros(tamanho).inteiro + cont
-		
-		const metrosUsadosBarra = proxDivisor3 - Number(`${trataNumeros(tamanho).inteiro}.${trataNumeros(tamanho).fracao}`);
-		sobra = metrosUsadosBarra
-		sobra = sobra.toFixed(2)
-
-		return sobra
-	}
-	
-}
-
-console.log(calculaSobra(19.1));
-//falta implementar a logica para quando o numero fracionario tiver como
-//divisor de 3 em sua parte inteira. 
-//OBS condição tem que ser enquanto o numero for diferente dele mesmo (x !==x) e x % 3 !== 0
+ */
